b08303ca6182f8be48b8f7a9448ed96ea68dc39f
Ankush <ankushrawat950@gmail.com>
2019-06-27 14:47:39 +0530

    added slideshow, auto typed texts

--- javascript/index_js.js
+++ javascript/index_js.js
@@ -1,28 +1,20 @@
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+//jqueryh after page loaded
+$(function(){
+  // jQuery methods go here...
+  $('#slides').superslides({
+    hashchange: true,
+    play: 2000,
+});
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
+var typed = new Typed(".typed", {
+  strings: ["Android Developer.", "Web Developer.", "Student."],
+  typeSpeed: 70,
+  loop: true,
+  startDelay: 1000,
+  showCursor: false
+});
+
+
+});
