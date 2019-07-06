354029e6e5705952b48b8853519ffc868c2bebc5
Ankush <ankushrawat950@gmail.com>
2019-07-06 11:59:36 +0530

    added blast, using bootstrap  for image handling in about me

--- javascript/index_js.js
+++ javascript/index_js.js
@@ -6,16 +6,59 @@ $(function(){
     hashchange: true,
     play: 2000,
     animation: 'fade'
-});
+  });
 
 
-var typed = new Typed(".typed", {
-  strings: ["Android Developer.", "Web Developer.", "Student."],
-  typeSpeed: 70,
-  loop: true,
-  startDelay: 1000,
-  showCursor: false
-});
+  var typed = new Typed(".typed", {
+    strings: ["Android Developer.", "Web Developer.", "Student."],
+    typeSpeed: 70,
+   loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+ 
+  //using blast for headings
+  $('h3').blast({
+    delimiter: 'character'
+  });
+
+  $('h3').on('mouseenter', '.blast', function(){
+
+    // Store the targeted `.blast` element as an instanced variable
+    var $target = $(this);
+
+    // Add the animation classes to it
+    $target.addClass('animated bounce');
+    
+    // Run the `removeClass` function on our instanced variable after
+    // 1 sec (animation length of `.bounce`)
+    setTimeout(function(){
+      $target.removeClass('animated bounce');
+    }, 1000);
+  });
+
+
+  //setting up blast for paragraphs
+  $('p').blast({
+    delimiter: 'word'
+  });
+
+  $('p').on('mouseenter', '.blast', function(){
+
+    // Store the targeted `.blast` element as an instanced variable
+    var $target = $(this);
+
+    // Add the animation classes to it
+    $target.addClass('animated bounce');
+    
+    // Run the `removeClass` function on our instanced variable after
+    // 1 sec (animation length of `.bounce`)
+    setTimeout(function(){
+      $target.removeClass('animated bounce');
+    }, 1000);
+  });
+
+
 
 
 });
@@ -31,3 +74,4 @@ $(window).on("load", function () {
 
 
 });
+
