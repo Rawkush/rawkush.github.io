953149c3adb8afc2d9bddc58c62013c4ed2fe044
Ankush <ankushrawat950@gmail.com>
2019-07-08 20:04:24 +0530

    pie charts for different screen size defined

--- javascript/index_js.js
+++ javascript/index_js.js
@@ -18,9 +18,10 @@ $(function () {
     showCursor: false
   });
 
+  
 
-  createChart();
-
+  createChart('.chart', 152);
+  createChart('.smallchart', 90, 5);
 
 
 });
@@ -36,24 +37,404 @@ $(document).ready(function () {
   doBlast('h3', 'character');
   doBlast('p', 'word');
   doBlast('.main', 'character');
-
+  responsiveChartDisplay()
 
 });
 
 
-function createChart() {
-  $('.chart').easyPieChart({
+function createChart(element, Size, lineWidth = 10) {
+  $(element).easyPieChart({
     easing: 'easeInOut',
     barColor: '#08fdd8',
     trackColor: false,
     scaleColor: false,
-    lineWidth: 10,
-    size: 152,
+    lineWidth: lineWidth,
+    size: Size,
     onStep: function (from, to, percent) {
       $(this.el).find('.percent').text(Math.round(percent));
     }
+  });
+}
+
+function responsiveChartDisplay() {
+
+  $(window).resize(function () {
+    console.log($(window).width());
+
+    reSize();
 
   });
 
-  
+
 }
+
+function reSize() {
+
+ /*  if ($(window).width() < 913) {
+
+    $("#java-chart").removeClass('col-md-4').addClass('col-md-12');
+    $("#js-chart").remove();
+    $("#python-chart").remove();
+  } else
+    if ($(window).width() < 983) {
+      // show only two charts
+      $("#js-chart").removeClass('col-md-4').addClass('col-md-6');
+      $("#java-chart").removeClass('col-md-4').addClass('col-md-6');
+      $("#python-chart").remove();
+
+    } else {
+      if (!($("#python-chart").length && $("#js-chart").length)) {
+        $("#pie-container").remove();
+        $("#container").append(data);
+
+      }
+    } */
+
+
+}
+
+
+function display3BigCharts() {
+  let allCharts = `
+   <div id="pie-container" class="row no-gutters">
+
+                        <div id="java-chart" class="col-md-4">
+                            <div class="box">
+                                <div class="chart" data-percent="73">
+                                    <span class="percent">73%</span>
+                                    <canvas height="152" width="152"></canvas>
+                                </div>
+
+                                <span>
+                                    <h3>java</h3>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div id="js-chart" class="col-md-4">
+                            <div class="box">
+                                <div class="chart" data-percent="73">
+                                    <span class="percent">73%</span>
+                                    <canvas height="152" width="152"></canvas>
+                                </div>
+
+                                <span>
+                                    <h3>javascript</h3>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div id="python-chart" class="col-md-4">
+                            <div class="box">
+                                <div class="chart" data-percent="73">
+                                    <span class="percent">73%</span>
+                                    <canvas height="152" width="152"></canvas>
+                                </div>
+
+                                <span>
+                                    <h3>python</h3>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div id="small-container" class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="box">
+                                        <div class="smallchart" data-percent="73">
+                                            <span class="percent">73%</span>
+                                            <canvas height="152" width="152"></canvas>
+                                        </div>
+
+                                        <span>
+                                            <h3>C++</h3>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-3">
+                                    <div class="box">
+                                        <div class="smallchart" data-percent="73">
+                                            <span class="percent">73%</span>
+                                            <canvas height="152" width="152"></canvas>
+                                        </div>
+
+                                        <span>
+                                            <h3>C </h3>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="box">
+                                        <div class="smallchart" data-percent="73">
+                                            <span class="percent">73%</span>
+                                            <canvas height="152" width="152"></canvas>
+                                        </div>
+
+                                        <span>
+                                            <h3>C#</h3>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-3">
+                                    <div class="box">
+                                        <div class="smallchart" data-percent="73">
+                                            <span class="percent">73%</span>
+                                            <canvas height="152" width="152"></canvas>
+                                        </div>
+
+                                        <span>
+                                            <h3>MySql</h3>
+                                        </span>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+
+                        <div id="scre" class="col-md-12">
+
+
+                        </div>
+
+                    </div>`
+
+  return allCharts;
+}
+
+
+function display2BigCharts() {
+
+  let chart = `
+  <div id="pie-container" class="row no-gutters">
+
+      <div id="java-chart" class="col-md-6">
+          <div class="box">
+              <div class="chart" data-percent="73">
+                  <span class="percent">73%</span>
+                  <canvas height="152" width="152"></canvas>
+              </div>
+
+              <span>
+                  <h3>java</h3>
+              </span>
+          </div>
+      </div>
+
+      <div id="js-chart" class="col-md-6">
+          <div class="box">
+              <div class="chart" data-percent="73">
+                  <span class="percent">73%</span>
+                  <canvas height="152" width="152"></canvas>
+              </div>
+
+              <span>
+                  <h3>javascript</h3>
+              </span>
+          </div>
+      </div>
+
+
+
+      <div id="small-container" class="col-md-12">
+          <div class="row">
+              <div class="col-md-4">
+                  <div class="box">
+                      <div class="smallchart" data-percent="73">
+                          <span class="percent">73%</span>
+                          <canvas height="152" width="152"></canvas>
+                      </div>
+
+                      <span>
+                          <h3>C++</h3>
+                      </span>
+                  </div>
+              </div>
+
+              <div class="col-md-4">
+                  <div class="box">
+                      <div class="smallchart" data-percent="73">
+                          <span class="percent">73%</span>
+                          <canvas height="152" width="152"></canvas>
+                      </div>
+
+                      <span>
+                          <h3>C </h3>
+                      </span>
+                  </div>
+              </div>
+              <div class="col-md-4">
+                  <div class="box">
+                      <div class="smallchart" data-percent="73">
+                          <span class="percent">73%</span>
+                          <canvas height="152" width="152"></canvas>
+                      </div>
+
+                      <span>
+                          <h3>C#</h3>
+                      </span>
+                  </div>
+              </div>
+
+              <div class="col-md-4">
+                  <div class="box">
+                      <div class="smallchart" data-percent="73">
+                          <span class="percent">73%</span>
+                          <canvas height="152" width="152"></canvas>
+                      </div>
+
+                      <span>
+                          <h3>MySql</h3>
+                      </span>
+                  </div>
+              </div>
+
+              <div class="col-md-4">
+                  <div id="python-chart">
+                      <div class="box">
+                          <div class="smallchart" data-percent="73">
+                              <span class="percent">73%</span>
+                              <canvas height="152" width="152"></canvas>
+                          </div>
+                          <span>
+                              <h3>python</h3>
+                          </span>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+
+      </div>
+
+      <div id="smallest-chart" class="col-md-12">
+          <div class="row">
+
+
+
+          </div>
+
+      </div>
+
+  </div>
+`
+
+
+  return chart;
+}
+
+function display1BigCharts(){
+  let chart=`     
+          <div id="pie-container" class="row no-gutters">
+
+  <div id="java-chart" class="col-md-12">
+      <div class="box">
+          <div class="chart" data-percent="73">
+              <span class="percent">73%</span>
+              <canvas height="152" width="152"></canvas>
+          </div>
+
+          <span>
+              <h3>java</h3>
+          </span>
+      </div>
+  </div>
+
+
+
+  <div id="small-container" class="col-md-12">
+      <div class="row">
+          <div class="col-md-4">
+              <div class="box">
+                  <div class="smallchart" data-percent="73">
+                      <span class="percent">73%</span>
+                      <canvas height="152" width="152"></canvas>
+                  </div>
+
+                  <span>
+                      <h3>C++</h3>
+                  </span>
+              </div>
+          </div>
+
+          <div class="col-md-4">
+              <div class="box">
+                  <div class="smallchart" data-percent="73">
+                      <span class="percent">73%</span>
+                      <canvas height="152" width="152"></canvas>
+                  </div>
+
+                  <span>
+                      <h3>C </h3>
+                  </span>
+              </div>
+          </div>
+          <div class="col-md-4">
+              <div class="box">
+                  <div class="smallchart" data-percent="73">
+                      <span class="percent">73%</span>
+                      <canvas height="152" width="152"></canvas>
+                  </div>
+
+                  <span>
+                      <h3>C#</h3>
+                  </span>
+              </div>
+          </div>
+
+          <div class="col-md-4">
+              <div class="box">
+                  <div class="smallchart" data-percent="73">
+                      <span class="percent">73%</span>
+                      <canvas height="152" width="152"></canvas>
+                  </div>
+
+                  <span>
+                      <h3>MySql</h3>
+                  </span>
+              </div>
+          </div>
+
+          <div class="col-md-4">
+              <div id="python-chart">
+                  <div class="box">
+                      <div class="smallchart" data-percent="73">
+                          <span class="percent">73%</span>
+                          <canvas height="152" width="152"></canvas>
+                      </div>
+                      <span>
+                          <h3>python</h3>
+                      </span>
+                  </div>
+              </div>
+          </div>
+
+          <div id="js-chart" class="col-md-4">
+              <div class="box">
+                  <div class="smallchart" data-percent="73">
+                      <span class="percent">73%</span>
+                      <canvas height="152" width="152"></canvas>
+                  </div>
+
+                  <span>
+                      <h3>javascript</h3>
+                  </span>
+              </div>
+          </div>
+
+
+      </div>
+
+
+  </div>
+
+
+</div>`
+
+  return chart;
+}
+
