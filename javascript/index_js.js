99910db165cf335b8a5fa2997a52c2250820ec5e
Ahraar Alam <ahraralam2012@gmail.com>
2019-07-10 02:21:39 +0530

    reformate code

--- javascript/index_js.js
+++ javascript/index_js.js
@@ -1,12 +1,11 @@
 //jquery after page loaded
-$(function () {
+$(function() {
   // jQuery methods go here...
-  $('#slides').superslides({
+  $("#slides").superslides({
     hashchange: true,
     play: 2000,
-    animation: 'fade'
+    animation: "fade"
   });
-
 
   var typed = new Typed(".typed", {
     strings: ["Android Developer.", "Web Developer.", "Programmer."],
@@ -17,34 +16,29 @@ $(function () {
   });
 
   reSize();
-
-
 });
 
-
 //runs after all resources are loaded
-$(document).ready(function () {
+$(document).ready(function() {
   // hiding the loader
-  $(".loader .inner").fadeOut(500, function () {
+  $(".loader .inner").fadeOut(500, function() {
     $(".loader").fadeOut(750);
   });
 
-  doBlast('h3', 'character');
-  doBlast('p', 'word');
-  doBlast('.main', 'character');
-  responsiveChartDisplay()
+  doBlast("h3", "character");
+  doBlast("p", "word");
+  doBlast(".main", "character");
+  responsiveChartDisplay();
 
   // for navbar
 
-  $("#navbar li a").click(function (e) {
+  $("#navbar li a").click(function(e) {
     e.preventDefault();
 
     var targetElement = $(this).attr("href");
     var targetPosition = $(targetElement).offset().top;
     $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
-
-});
-
+  });
 
   const nav = $("#navbar");
   const navTop = nav.offset().top;
@@ -52,86 +46,71 @@ $(document).ready(function () {
   $(window).on("scroll", stickyNavigation);
 
   function stickyNavigation() {
-
     var body = $("body");
 
     if ($(window).scrollTop() >= navTop) {
       body.css("padding-top", nav.outerHeight() + "px");
       body.addClass("fixedNav");
-    }
-    else {
+    } else {
       body.css("padding-top", 0);
       body.removeClass("fixedNav");
     }
-
   }
-
-
 });
-
 
 function createChart(element, Size, lineWidth = 10) {
   $(element).easyPieChart({
-    easing: 'easeInOut',
-    barColor: '#08fdd8',
+    easing: "easeInOut",
+    barColor: "#08fdd8",
     trackColor: false,
     scaleColor: false,
     lineWidth: lineWidth,
     size: Size,
-    onStep: function (from, to, percent) {
-      $(this.el).find('.percent').text(Math.round(percent));
+    onStep: function(from, to, percent) {
+      $(this.el)
+        .find(".percent")
+        .text(Math.round(percent));
     }
   });
 }
 
 function responsiveChartDisplay() {
-  $(window).resize(function () {
+  $(window).resize(function() {
     console.log($(window).width());
     reSize();
   });
 }
 
 function reSize() {
-
   if ($(window).width() < 856) {
     // smallest
     if (!$("#java-chart").length) {
       $("#container").append(display1BigCharts());
-    } else
-
-      if ($("#python-chart").length || $("#js-chart").length) {
-        $("#pie-container").remove();
-        $("#container").append(display1BigCharts());
-      }
-  } else
-    if ($(window).width() < 966) {
-      // show only two charts
-      if (!$("#java-chart").length) {
-        $("#container").append(display2BigCharts());
-      } else
-
-        if ($("#python-chart").length) {
-          $("#pie-container").remove();
-          $("#container").append(display2BigCharts());
-        }
-
-    } else {
-      //biggest
-      if (!$("#java-chart").length) {
-        $("#container").append(display3BigCharts());
-      } else
-
-        if ((!$("#python-chart").length) || (!$("#js-chart").length)) {
-          $("#pie-container").remove();
-          $("#container").append(display3BigCharts());
-        }
+    } else if ($("#python-chart").length || $("#js-chart").length) {
+      $("#pie-container").remove();
+      $("#container").append(display1BigCharts());
     }
+  } else if ($(window).width() < 966) {
+    // show only two charts
+    if (!$("#java-chart").length) {
+      $("#container").append(display2BigCharts());
+    } else if ($("#python-chart").length) {
+      $("#pie-container").remove();
+      $("#container").append(display2BigCharts());
+    }
+  } else {
+    //biggest
+    if (!$("#java-chart").length) {
+      $("#container").append(display3BigCharts());
+    } else if (!$("#python-chart").length || !$("#js-chart").length) {
+      $("#pie-container").remove();
+      $("#container").append(display3BigCharts());
+    }
+  }
 
-  createChart('.chart', 152);
-  createChart('.smallchart', 90, 5);
-
+  createChart(".chart", 152);
+  createChart(".smallchart", 90, 5);
 }
-
 
 function display3BigCharts() {
   let allCharts = `
@@ -236,14 +215,12 @@ function display3BigCharts() {
 
                       
 
-                    </div>`
+                    </div>`;
 
   return allCharts;
 }
 
-
 function display2BigCharts() {
-
   let chart = `
   <div id="pie-container" class="row no-gutters">
 
@@ -348,8 +325,7 @@ function display2BigCharts() {
       </div>
 
   </div>
-`
-
+`;
 
   return chart;
 }
@@ -460,8 +436,7 @@ function display1BigCharts() {
   </div>
 
 
-</div>`
+</div>`;
 
   return chart;
 }
-
